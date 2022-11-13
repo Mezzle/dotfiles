fcacadd0f818dbbb2550967f992a2f02f4436ad4
Martin Meredith <martin@sourceguru.net>
2022-11-13 09:00:38 +0000

    Cleanup

--- private_dot_zshrc.d/private_99-brew.zsh
+++ private_dot_zshrc.d/private_99-brew.zsh
@@ -1,3 +1,3 @@
-    alias brewup='brew update; brew upgrade; brew upgrade --cask; brew cleanup; brew doctor'
-    export HOMEBREW_NO_ENV_HINTS=true
-    export PATH=$(brew --prefix)/sbin:$(brew --prefix)/bin:$PATH
+alias brewup='brew update; brew upgrade; brew upgrade --cask; brew cleanup; brew doctor'
+export HOMEBREW_NO_ENV_HINTS=true
+export PATH=$(brew --prefix)/sbin:$PATH
