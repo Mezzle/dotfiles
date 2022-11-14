804305934b1e8a62dd51e8f63614ec638f5c0cf7
Martin Meredith <martin.meredith@substrakthealth.com>
2022-11-14 09:59:46 +0000

    Update brew path

--- private_dot_zshrc.d/private_99-brew.zsh
+++ private_dot_zshrc.d/private_99-brew.zsh
@@ -1,3 +1,3 @@
     alias brewup='brew update; brew upgrade; brew upgrade --cask; brew cleanup; brew doctor'
     export HOMEBREW_NO_ENV_HINTS=true
-    export PATH=/usr/local/sbin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
+    export PATH=$(brew --prefix)/sbin:$(brew --prefix)/bin:$PATH
