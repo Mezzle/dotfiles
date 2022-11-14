443ddcfe6a7fb61db40288dfdbf241c676f7ae91
Martin Meredith <martin.meredith@substrakthealth.com>
2022-11-14 09:57:17 +0000

    Add brew and install age

---
+++ private_dot_zshrc.d/private_99-brew.zsh
@@ -0,0 +1,3 @@
+    alias brewup='brew update; brew upgrade; brew upgrade --cask; brew cleanup; brew doctor'
+    export HOMEBREW_NO_ENV_HINTS=true
+    export PATH=/usr/local/sbin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
