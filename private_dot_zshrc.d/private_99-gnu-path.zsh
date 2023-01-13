33e055e964e1937edab349d348a6852d6589dc26
Martin Meredith <martin.meredith@substrakthealth.com>
2023-01-13 15:56:18 +0000

    Cleanup GNU tools usage

--- private_dot_zshrc.d/private_99-gnu-path.zsh
+++ private_dot_zshrc.d/private_99-gnu-path.zsh
@@ -1,6 +1,9 @@
-export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/gnu-which/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+{{ if eq .chezmoi.os "darwin" -}}
+export BREW_PREFIX=$(brew --prefix)
+export PATH="${BREW_PREFIX}/opt/grep/libexec/gnubin:${PATH}"
+export PATH="${BREW_PREFIX}/opt/gnu-which/libexec/gnubin:${PATH}"
+export PATH="${BREW_PREFIX}/opt/coreutils/libexec/gnubin:${PATH}"
+export MANPATH="${BREW_PREFIX}/opt/coreutils/libexec/gnuman:${MANPATH}"
 
 alias ls="gls --color=always"
+{{ end -}}
