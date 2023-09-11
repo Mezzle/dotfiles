903a421f22d8ab3f6f2d8f43bdb939e62af2ad1a
Martin Meredith <martin@sourceguru.net>
2023-09-11 23:59:00 +0100

    Gizmo reinstall updates

---
+++ private_dot_zshrc.d/99-bun.zsh
@@ -0,0 +1,6 @@
+# bun completions
+[ -s "/Users/mez/.bun/_bun" ] && source "/Users/mez/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
--- private_dot_zshrc.d/private_99-fnm.zsh
+++ private_dot_zshrc.d/private_99-fnm.zsh
@@ -1,1 +1,2 @@
 eval "$(fnm env --use-on-cd)"
+eval "$(fnm completions --shell zsh)"
