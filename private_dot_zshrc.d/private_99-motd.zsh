b0011d56287910bc63996b532ccd72449e63da4b
Martin Meredith <martin@sourceguru.net>
2025-10-20 20:45:26 +0100

    Update dotfiles for re-install

--- private_dot_zshrc.d/private_99-motd.zsh
+++
@@ -1,7 +0,0 @@
-if [ -f /etc/motd ]; then
-    cmp -s $HOME/.hushlogin /etc/motd
-    if [ $? != 0 ]; then
-        tee $HOME/.hushlogin < /etc/motd
-        echo -n "Press Enter to continue: " && read ans
-    fi
-fi
